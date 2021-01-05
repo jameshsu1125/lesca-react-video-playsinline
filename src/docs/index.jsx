7a46e458ea768ed541beca9732d75b0908ec5af2
James Hsu <70932507+jameshsu1125@users.noreply.github.com>
2021-01-05 18:30:09 +0800

    Update index.jsx

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -4,8 +4,20 @@ import MyClass from './../lib/index';
 
 import './styles.css';
 
+function update(e, t, d) {
+	//console.log(e, t, d);
+}
+
 function Demo() {
-	return <MyClass url={{ mp4: 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4' }} width={window.innerWidth} height={window.innerHeight} />;
+	return (
+		<MyClass
+			fps={24}
+			url={{ mp4: 'https://dlcdnwebimgs.asus.com/files/media/6570DD19-B43C-4496-9A24-53CBFB320D9B/vid/enter/video/20201224_Intro_Video_Mobile.mp4' }}
+			width={window.innerWidth}
+			height={window.innerHeight}
+			onupdate={update}
+		/>
+	);
 }
 
 render(<Demo />, document.getElementById('app'));
